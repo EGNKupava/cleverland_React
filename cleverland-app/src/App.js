523b729cbd87630605851924941d72d69dbb4aae
EGNKupava <kupavaegn@gmail.com>
2020-10-11 16:41:34 +0300

    weather

--- cleverland-app/src/App.js
+++ cleverland-app/src/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Link
+} from "react-router-dom";
+
 import ProductList from './components/productList/ProductList';
+import { Weather } from './components/weather/weather';
+
+import './App.css';
 
 const App = () => (
-  <div className="App">
-    <h1 className="main-title">Cleverland Practice</h1>
-    <div>
-      <ProductList className="ProductList" />
+  <Router>
+    <div className="App">
+      <Link className="logo" to="/">
+      <h1 className="main-title">Cleverland Practice</h1>
+      </Link>
+      <div className="nav-block">
+        <NavLink to="/products-list">Список продуктов</NavLink>
+        <NavLink to="/weather">Погода</NavLink>
+      </div>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <p className="description">Стартовая страница домашних заданий Cleverland</p>
+            <div style={{textAlign: "center"}}>
+               <img src="https://cleverland.by/images/ART.png"/>
+            </div>
+          </Route>
+          <Route path="/products-list">
+            <ProductList className="ProductList" />
+          </Route>
+          <Route path="/weather">
+            <Weather />
+          </Route>
+        </Switch>
+      </div>
     </div>
-  </div>
-  );
+  </Router>
+);
 
 
 export default App;
